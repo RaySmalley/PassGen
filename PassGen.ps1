b0f7f5df6e69d59e2f44ab373e5d89356ae29daf
RaySmalley <raysmalley@hotmail.com>
2023-02-23 17:01:58 -0500

    Update PassGen.ps1

--- PassGen.ps1
+++ PassGen.ps1
@@ -2,6 +2,36 @@
 ### Ray Smalley        ###
 ### 2018               ###
 
+# Disable progress bar for faster downloads
+$global:ProgressPreference = 'SilentlyContinue'
+
+# Download function
+function Download {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][string]$URL,
+	    [Parameter(Mandatory)][string]$Name,
+	    [Parameter()][string]$Filename = $(if ($URL -match "\....$") {(Split-Path $URL -Leaf)}),
+        [Parameter()][string]$OutputPath = $env:TEMP
+	)
+    if (-Not($Filename)) {
+        Write-Warning "Filename parameter needed. Download failed."
+        Write-Host
+        Break
+    }
+    $Output = $OutputPath + "\$Filename"
+    #$Name = $Name -csplit '(?=[A-Z])' -ne '' -join ' '
+    #Write-Host "Downloading $Name..."`n
+    $Error.Clear()
+    if (!(Test-Path $Output)) {(New-Object System.Net.WebClient).DownloadFile($URL, $Output)}
+    if ($Error.count -gt 0) {Write-Host "Retrying..."`n; $Error.Clear(); (New-Object System.Net.WebClient).DownloadFile($URL, $Output)}
+    if ($Error.count -gt 0) {Write-Warning "$Name download failed";Write-Host}
+    New-Variable -Name $Name"Output" -Value $Output -Scope Global -Force
+}
+
+Download -Name WordList -URL https://github.com/RaySmalley/PassGenWords/raw/main/WordList.txt
+$WordList = Get-Content $WordListOutput
+
 function pg {
     Param(
         [ValidateRange(1,99999)][Int]$Size = 12,
@@ -27,4 +57,30 @@ function pg {
     Set-Clipboard $PW
     Write-Host "Password added to clipboard: " -ForegroundColor Cyan -NoNewline
     Write-Host "$PW"`n -ForegroundColor Red
+}
+
+function pgw {
+    $FirstWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random ($WordList | where {$_.Length -gt 4})))
+    $SecondWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random ($WordList | where {$_.Length -gt 4})))
+    $ThirdWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random ($WordList | where {$_.Length -gt 4})))
+    Write-Host "Password added to clipboard: " -ForegroundColor Cyan -NoNewline
+    Write-Host $FirstWord -ForegroundColor Red -NoNewline
+    Write-Host - -NoNewline
+    Write-Host $SecondWord -ForegroundColor Yellow -NoNewline
+    Write-Host - -NoNewline
+    Write-Host $ThirdWord -ForegroundColor Green
+    Set-Clipboard $FirstWord-$SecondWord-$ThirdWord
+}
+
+function pge {
+    $FirstWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random ($WordList | where {$_.Length -gt 4})))
+    $SecondWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random ($WordList | where {$_.Length -gt 4})))
+    $Symbol = @('@','!','#','$','%','^','&','*','-','_','=','+','|',';',':','<','>',',','.','?','/') | Get-Random
+    $Number = Get-Random -Minimum 0 -Maximum 10
+    Write-Host "Password added to clipboard: " -ForegroundColor Cyan -NoNewline
+    Write-Host $FirstWord -ForegroundColor Red -NoNewline
+    Write-Host $Symbol -NoNewline -ForegroundColor White
+    Write-Host $SecondWord -ForegroundColor Yellow -NoNewline
+    Write-Host $Number -ForegroundColor Green
+    Set-Clipboard $FirstWord$Symbol$SecondWord$Number
 }
